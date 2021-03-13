4a015c53abc731a5058c910cd2189a9d518fe107
Mridul Malviya <mridul.malviya@laerdal.com>
2021-03-13 15:25:11 +0530

    Added Media Manager

--- HTTPLiveStreaming.Android/MainActivity.cs
+++ HTTPLiveStreaming.Android/MainActivity.cs
@@ -6,10 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using MediaManager;
+using Android.Content;
 
 namespace HTTPLiveStreaming.Droid
 {
-    [Activity(Label = "HTTPLiveStreaming", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "HTTPLiveStreaming", Icon = "@mipmap/icon", LaunchMode = LaunchMode.SingleTop, Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -21,6 +23,7 @@ namespace HTTPLiveStreaming.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CrossMediaManager.Current.Init(this);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -28,6 +31,16 @@ namespace HTTPLiveStreaming.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        /// <summary>
+        /// If you want to handle when the app is opened via a notification tap, you can override OnNewIntent on your activity:
+        /// </summary>
+        /// <param name="intent"></param>
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            // TODO: Handle app opened from notification tap.
         }
     }
 }
--- HTTPLiveStreaming/MainPage.xaml.cs
+++ HTTPLiveStreaming/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediaManager;
+using MediaManager.Library;
 using Xamarin.Forms;
 
 namespace HTTPLiveStreaming
@@ -13,6 +15,23 @@ namespace HTTPLiveStreaming
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            CrossMediaManager.Current.MediaPlayer.AutoAttachVideoView = false;
+            _ = PlayVideo();
+
+
+        }
+
+        private async Task PlayVideo()
+        {
+            var item = await CrossMediaManager.Current.Extractor.CreateMediaItem("https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8");
+            item.MediaType = MediaType.Hls;
+
+            await CrossMediaManager.Current.Play(item);
         }
     }
 }
